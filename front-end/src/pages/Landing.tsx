ee93696e150b782575e061a1b8db427924453505
Kyle Kirsten <kylejkirsten@gmail.com>
2019-08-05 16:36:54 -0400

    Landing page style modifications

--- front-end/src/pages/Landing.tsx
+++ front-end/src/pages/Landing.tsx
@@ -12,18 +12,14 @@ export default class Landing extends React.Component<IProps,IState>{
 
     render() {
         return(
-            <>
-            <Carousel controls = {false} className = "landing-background-carousel">
+            <div className = 'landing-background'>
+            <Carousel controls = {false} indicators = {false} className = "landing-background-carousel">
                     <Carousel.Item>
                         <img
                         className="d-block w-100"
                         src="http://writingexercises.co.uk/images2/randomimage/balloons.jpg"
                         alt="First slide"
                         />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -31,10 +27,6 @@ export default class Landing extends React.Component<IProps,IState>{
                         src="http://writingexercises.co.uk/images2/randomimage/trafalgar.jpg"
                         alt="Third slide"
                         />
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -42,23 +34,23 @@ export default class Landing extends React.Component<IProps,IState>{
                         src="http://writingexercises.co.uk/images2/randomimage/waterfall.jpg"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             <Container className = "landing-container">
                 
-                <Row><h1>Kutsies</h1></Row>
-                <Row><p>Sample text about application...</p></Row>
                 <Row>
+                    <h1>Kutsies</h1>
+                    <hr className = 'hr-light'/>
+                </Row>
+                <Row><p className = 'landing-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis officia voluptatem nulla nemo temporibus accusantium inventore, labore consequuntur suscipit voluptatum incidunt sed quisquam, esse dicta qui adipisci doloremque. Deserunt, vel!
+                Quod quas nam quos saepe quisquam culpa tempore dolor laboriosam animi? Rem consequuntur et explicabo quaerat quae qui quidem ea! Minima debitis quod quae pariatur nulla dignissimos labore in ab?
+                Itaque sed quis reiciendis? Modi labore voluptatem enim error, vero esse quidem voluptas nulla iusto beatae velit dignissimos sunt veritatis quam adipisci magnam suscipit totam tempora asperiores doloremque odit maiores.</p></Row>
+                <Row className = 'landing-buttons'>
                     <Button>Make</Button>
                     <Button>Work</Button>
                 </Row>
             </Container>
-            </>
+            </div>
         )
     }
 }
