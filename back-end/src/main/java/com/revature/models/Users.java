06839befe2592a3977adce67aba33acba82c142e
Kyle Kirsten <kylejkirsten@gmail.com>
2019-08-12 16:07:24 -0400

    Trying to fix Job Join Statements (backend)

--- back-end/src/main/java/com/revature/models/Users.java
+++ back-end/src/main/java/com/revature/models/Users.java
@@ -2,6 +2,7 @@ package com.revature.models;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
