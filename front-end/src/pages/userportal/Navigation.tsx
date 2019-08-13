b4838a684969078dfc6b96e6b52c75af83c7a22d
Jae Nwawe <nwawe.jae@gmail.com>
2019-08-12 20:19:30 -0400

    dropdown User button on parent navigation bar in userPortal

--- front-end/src/pages/userportal/Navigation.tsx
+++ front-end/src/pages/userportal/Navigation.tsx
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
-export default class LeftUserPortal extends Component <any, any>{
+export default class LeftUserPortal extends Component<any, any>{
 
     render() {
-        return(  
+        return (
             <React.Fragment>
                 <Navbar bg="light">
                     <Navbar.Brand>Kutsies</Navbar.Brand>
                     <Nav className="mr-auto">
-                        <Nav.Link as={ Link } to="/">Home</Nav.Link>
-                        <Nav.Link as={ Link } to="/userportal/search">Search</Nav.Link>
-                        <Nav.Link as={ Link } to="/userportal/myjobs">My Jobs</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/userportal/search">Search</Nav.Link>
+                        <Nav.Link as={Link} to="/userportal/myjobs">My Jobs</Nav.Link>
                     </Nav>
                     <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
-                        Signed in as: <a >Current User</a>
+                            Signed in as: <a >Current User</a>
                         </Navbar.Text>
-                    </Navbar.Collapse>    
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Current User
+  </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">My Account</Dropdown.Item>
+                                //The account page will modify the information stored in the User Class
+                                <Dropdown.Item href="#/action-2">My Jobs</Dropdown.Item>
+
+                                <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Navbar.Collapse>
                 </Navbar>
             </React.Fragment>
         );
-    } 
+    }
 }
