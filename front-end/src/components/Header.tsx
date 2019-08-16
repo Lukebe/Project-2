959bf0c1fdcfb0dd4a63c83398387a5c1441a7b7
Jae Nwawe <nwawe.jae@gmail.com>
2019-08-16 19:32:53 -0400

    Header.tsx Links and Kutsie logo

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -46,21 +46,21 @@ class Header extends Component<IAuthProps, IState>{
     render() {
         return (
             <>
+                <div className="headerLinks" >
+                    <Navbar bg="light" expand="lg">
+                        <Navbar.Brand href="#home">Kutsies</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <img src={iconWhite} style={{ width: 100, marginTop: -7 }} />
+                            <Nav className="mr-auto">
 
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">Kutsies</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <img src={iconWhite} style={{ width: 100, marginTop: -7 }} />
-                        <Nav className="mr-auto">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#userPortal">User Portal</Nav.Link>
 
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#userPortal">User Portal</Nav.Link>
+                                <Nav.Link href="#makerportal">Maker Portal</Nav.Link>
+                                <Nav.Link href="#userAccount">My Account</Nav.Link>
 
-                            <Nav.Link href="#makerportal">Maker Portal</Nav.Link>
-                            <Nav.Link href="#userAccount">My Account</Nav.Link>
-
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <Nav.Link href="#userPortal">User Portal</Nav.Link>
 
 
@@ -69,14 +69,15 @@ class Header extends Component<IAuthProps, IState>{
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Form inline>
+                            </NavDropdown> */}
+                            </Nav>
+                            {/* <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                             <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Navbar>
+                        </Form> */}
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
             </>
         )
     }
