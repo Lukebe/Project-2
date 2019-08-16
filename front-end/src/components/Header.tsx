f7c7c905e77c32c5d0d0a320ed9118f3ee0ca429
Jae Nwawe <nwawe.jae@gmail.com>
2019-08-16 19:06:07 -0400

    Header.tsx changes

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import iconWhite from '../resources/images/icon/icon-white-2.png';
 import { Row, Col, Nav, Navbar, FormControl, Button, NavDropdown, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -7,11 +8,13 @@ import { loginSuccessful } from '../actions/Authentication.action';
 import { IAuthState, IAppState } from '../reducers';
 import { connect } from 'react-redux';
 import { render } from 'react-dom';
+import src from '*.png';
 
 
 
 
 type IProps = IComponentProps & IAuthProps;
+
 
 export interface IAuthProps {
     //data from state store
@@ -29,23 +32,39 @@ interface IState {
 class Header extends Component<IAuthProps, IState>{
     constructor(props: any) {
         super(props);
+        // this.myRef = React.createRef();
         this.state = {
 
         };
     }
+
+
+    // Maker Portal, User Portal, My Account. Incorporate logo or plain "Kutsies" title (like in footer) on the left. 
+    //My Account link should be on right. User portal and maker portal should be in the center.
+
+
     render() {
         return (
             <>
 
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Kutsies</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
+                        <img src={iconWhite} style={{ width: 100, marginTop: -7 }} />
                         <Nav className="mr-auto">
+
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#userPortal">User Portal</Nav.Link>
+
+                            <Nav.Link href="#makerportal">Maker Portal</Nav.Link>
+                            <Nav.Link href="#userAccount">My Account</Nav.Link>
+
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <Nav.Link href="#userPortal">User Portal</Nav.Link>
+
+
+                                <NavDropdown.Item href="#action/3.1">Testing</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
